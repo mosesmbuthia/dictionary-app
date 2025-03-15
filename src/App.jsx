78514c4630b53586e29610137f49f12ce6b3e489
mosesmbuthia <mbuthiamoses435@gmail.com>
2025-03-15 22:59:52 +0300

    completed the build without styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 
 import { useState } from 'react'
 import './App.css'
-import { Button, TextInput } from "evergreen-ui"
+import { Button, Heading, Pane, TextInput } from "evergreen-ui"
 
 
 function App() {
   const [word, setWord] = useState("")
-  const [loading, setLoading] = useState("false")
+  const [loading, setLoading] = useState(false)
+  const [wordDetails, setWordDetails] = useState(null)
   const searchWord = async () => {
+    if (!word?.length) return
     setLoading(true)
     try {
+      const response = await fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + word)
 
+      const data = await response?.json();
+      console.log(data);
+      setWordDetails(data[0]);
     } catch (e) {
       console.error(e);
     } finally {
@@ -22,11 +28,35 @@ function App() {
 
   return (
     <div>
-    <h1>This is the start</h1>
+    <h1>Dictionary App</h1>
 
     <TextInput onChange={e => setWord(e.target.value)} value={word} placeholder='Enter a word to search'/>
 
-      <Button appearance='primary' onClick={searchWord} isLoading={loading} disabled={loading} >Search</Button>
+      <Button 
+      appearance='primary' 
+      onClick={searchWord} 
+      isLoading={loading} 
+      disabled={loading} >Search</Button>
+
+    {wordDetails?.meanings && (
+     <Pane>
+     <Heading>Meanings:</Heading>
+     {wordDetails.meanings.map((meaning, index) => (
+            <Pane key={index}>
+              <Heading>{meaning.partOfSpeech}</Heading>
+              <ul>
+                {meaning.definitions.map((def, i) => (
+                  <li key={i}>{def.definition}</li>
+                ))}
+              </ul>
+     
+   </Pane>
+     ))}
+    </Pane>
+  
+  )}
+
+   
     </div>
       
   )
